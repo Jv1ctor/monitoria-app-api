5d83f57fbb61195a1d2bc254bc4fcf6aaaa55c62
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-06-03 15:24:51 -0300

    feat(user): add in repository find by email

--- src/modules/user/interfaces/user-repository.port.ts
+++ src/modules/user/interfaces/user-repository.port.ts
@@ -3,4 +3,5 @@ import type { Prisma, User } from '@/generated/prisma/browser';
 export type UserRepositoryPort = {
   create: (data: Prisma.UserCreateInput) => Promise<User>;
   findByRegistration: (registration: string) => Promise<User | null>;
+  findByEmail: (email: string) => Promise<User | null>;
 };
--- src/modules/user/repository/index.ts
+++ src/modules/user/repository/index.ts
@@ -1,8 +1,10 @@
 import type { UserRepositoryPort } from '../interfaces/user-repository.port';
 import { create } from './core/create.core';
+import { findByEmail } from './core/find-by-email.core';
 import { findByRegistration } from './core/find-by-registration.core';
 
 export const userRepository: UserRepositoryPort = {
   create: create,
   findByRegistration: findByRegistration,
+  findByEmail: findByEmail,
 };
