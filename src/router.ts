d36a51c577cc7fa4af00de7ad0ff1749eecee407
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-06-10 00:35:37 -0300

    feat(user/student): add routes

--- src/router.ts
+++ src/router.ts
@@ -6,12 +6,15 @@ import { documentRouter } from './modules/document/document.router';
 import { frequencysRouter } from './modules/frequencys/frequencys.router';
 import { lessonRouter } from './modules/lesson/lesson.router';
 import { majorRouter } from './modules/major/major.router';
+import { studentRouter } from './modules/student/student.router';
 import { subjectRouter } from './modules/subject/subject.router';
+import { userRouter } from './modules/user/user.router';
 
 export const router = Router();
 
-// router.use('/user');
 router.use('/auth', authRouter);
+router.use('/user', userRouter);
+router.use('/student', studentRouter);
 router.use('/major', majorRouter);
 router.use('/document', documentRouter);
 router.use('/subject', subjectRouter);
